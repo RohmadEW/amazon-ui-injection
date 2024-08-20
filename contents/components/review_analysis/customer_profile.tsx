e0d319835f2484b92ae58ec91acadee54a2fc5e6
Rohmad Eko Wahyudi <eko@impstudio.co.id>
2024-08-20 18:22:31 +0700

    feat: customer profile

---
+++ contents/components/review_analysis/customer_profile.tsx
@@ -0,0 +1,17 @@
+export const ReviewAnalysisCustomerProfile = () => {
+  return (
+    <div className="border rounded-md">
+      <div className="px-4 py-2 font-bold border-b">Customer Profile</div>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Who</th>
+            <th>When</th>
+            <th>Where</th>
+            <th>What</th>
+          </tr>
+        </thead>
+      </table>
+    </div>
+  )
+}
--- contents/components/review_analysis/main.tsx
+++ contents/components/review_analysis/main.tsx
@@ -1,8 +1,13 @@
+import { ReviewAnalysisCustomerProfile } from "./customer_profile"
+
 export const ReviewAnalysisMain = () => {
   return (
     <div className="p-4">
       <div>
         <strong>Intellegent Review Analysis</strong>
+      </div>
+      <div className="grid grid-cols-2 gap-4 mt-2">
+        <ReviewAnalysisCustomerProfile />
       </div>
     </div>
   )
