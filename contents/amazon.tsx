96c08f374d1c201f003bc8a9408a329045f54f49
Rohmad Eko Wahyudi <eko@impstudio.co.id>
2024-08-20 18:46:57 +0700

    feat: fix btn class

--- contents/amazon.tsx
+++ contents/amazon.tsx
@@ -27,17 +27,19 @@ const PlasmoAmazon = () => {
   const [showReviewAnalysis, setShowReviewAnalysis] = useState(true)
 
   return (
-    <div className="w-full border border-gray-300 rounded-md mt-4 mx-[100px]">
-      <div className="bg-gray-100 border-t border-t-gray-300">
-        <div className="flex item-centers">
-          <div className="p-4 border-r w-2/12">
+    <div className="w-full border border-gray-300 rounded-md mt-4 mx-[20px]">
+      <div className="bg-gray-100 border-t">
+        <div className="flex item-centers divide-x">
+          <div className="p-4 w-2/12">
             <div className="text-lg font">UI Injection for Amazon</div>
             <div className="flex item-centers gap-2">
-              <button className="px-2 py-1 bg-blue-500">Sign Up</button>
-              <button className="px-2 py-1 bg-white">Log In</button>
+              <button className="btn btn-sm btn-primary">Sign Up</button>
+              <button className="btn btn-sm btn-outline btn-primary">
+                Log In
+              </button>
             </div>
           </div>
-          <div className="p-4 border-r w-4/12">
+          <div className="p-4 w-4/12">
             <Asin />
             <div>#2 In Cell Phone & Accessories</div>
             <div>
@@ -49,29 +51,29 @@ const PlasmoAmazon = () => {
             <ReviewCount />
             <div className="flex flex-wrap item-centers gap-4">
               <button
-                className={`px-3 py-1 bg-white border border-gray-300 rounded-md ${showReviewAnalysis ? "bg-blue-500 text-white" : ""}`}
+                className={`btn btn-sm font-normal ${showReviewAnalysis ? "btn-primary" : "bg-white border border-gray-300"}`}
                 onClick={() => setShowReviewAnalysis(!showReviewAnalysis)}>
                 Review Analysis
               </button>
-              <button className="px-3 py-1 bg-white border border-gray-300 rounded-md">
+              <button className="btn btn-sm font-normal bg-white border border-gray-300">
                 Listing Optimization
               </button>
-              <button className="px-3 py-1 bg-white border border-gray-300 rounded-md">
+              <button className="btn btn-sm font-normal bg-white border border-gray-300">
                 Q&A Analysis
               </button>
-              <button className="px-3 py-1 bg-white border border-gray-300 rounded-md">
+              <button className="btn btn-sm font-normal bg-white border border-gray-300">
                 Market Insight
               </button>
-              <button className="px-3 py-1 bg-white border border-blue-500 text-blue-500 rounded-md">
+              <button className="btn btn-sm font-normal bg-white border border-blue-500 text-blue-500">
                 Category Analysis
               </button>
-              <button className="px-3 py-1 bg-white border border-blue-500 text-blue-500 rounded-md">
+              <button className="btn btn-sm font-normal bg-white border border-blue-500 text-blue-500">
                 Competitive Analysis
               </button>
-              <button className="px-3 py-1 bg-white border border-blue-500 text-blue-500 rounded-md">
+              <button className="btn btn-sm font-normal bg-white border border-blue-500 text-blue-500">
                 Drill-Down Details
               </button>
-              <button className="px-3 py-1 bg-white border border-blue-500 text-blue-500 rounded-md">
+              <button className="btn btn-sm font-normal bg-white border border-blue-500 text-blue-500">
                 Review Download
               </button>
             </div>
