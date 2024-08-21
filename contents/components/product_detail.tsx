5cf21f099dbb3d4cab4fb5e9cffb02fb80468a7f
Rohmad Eko Wahyudi <eko@impstudio.co.id>
2024-08-21 11:01:50 +0700

    feat: scrape product detail

--- contents/components/product_detail.tsx
+++ contents/components/product_detail.tsx
@@ -2,46 +2,164 @@ import { useAtom } from "jotai"
 import { useEffect, useState } from "react"
 
 import { productAtom } from "~store/products"
-import type { Product } from "~types/products"
-
-import { ProductDetailTitle } from "./product_detail/title"
-
-interface ProductDetailMainProps {
-  product: Product
-  onChange: (product: Product) => void
-}
+import type { Product, ProductStar } from "~types/products"
 
 export const ProductDetailMain = () => {
   const [product, setProduct] = useAtom(productAtom)
 
   const getAsin = () => {
-    const inputElement = document.querySelector(
-      "input#asin"
-    ) as HTMLInputElement
-    setProduct({ ...product, asin: inputElement?.value })
-  }
+    const element = document.querySelector("input#asin") as HTMLInputElement
 
-  const getCategory = () => {
-    const ulElement = document.querySelector(
-      "ul.a-unordered-list.a-horizontal.a-size-small"
-    ) as HTMLUListElement
-
-    if (ulElement) {
-      const anchorElements = ulElement.querySelectorAll("a")
-      const textArray = Array.from(anchorElements).map(
-        (a) => a.textContent?.trim() || ""
-      )
-      const textWithCommas = textArray.join(", ")
+    if (element) {
       setProduct((prevProduct) => ({
         ...prevProduct,
-        category: textWithCommas
+        asin: element.value
       }))
     }
   }
 
+  const getName = () => {
+    const element = document.querySelector(
+      "input#productTitle"
+    ) as HTMLInputElement
+
+    if (element) {
+      setProduct((prevProduct) => ({
+        ...prevProduct,
+        name: element.value
+      }))
+    }
+  }
+
+  const getCategory = () => {
+    const element = document.querySelector(
+      "input#productCategory"
+    ) as HTMLInputElement
+
+    if (element) {
+      setProduct((prevProduct) => ({
+        ...prevProduct,
+        category: element.value
+      }))
+    }
+  }
+
+  const getDescription = () => {
+    const element = document.querySelector(
+      "div#productDescription"
+    ) as HTMLDivElement
+
+    if (element) {
+      setProduct((prevProduct) => ({
+        ...prevProduct,
+        description: element.textContent?.trim() || ""
+      }))
+    }
+  }
+
+  const getImageUrl = () => {
+    const element = document.querySelector(
+      "input#productImageUrl"
+    ) as HTMLInputElement
+
+    if (element) {
+      setProduct((prevProduct) => ({
+        ...prevProduct,
+        image_url: element.value
+      }))
+    }
+  }
+
+  const getPrice = () => {
+    const element = document.querySelector(
+      "input#priceValue"
+    ) as HTMLInputElement
+
+    if (element) {
+      setProduct((prevProduct) => ({
+        ...prevProduct,
+        price: parseFloat(element.value)
+      }))
+    }
+  }
+
+  const getRating = () => {
+    const element = document.querySelector(
+      "span#acrPopover .a-size-base.a-color-base"
+    ) as HTMLSpanElement
+
+    if (element) {
+      const rating = element.textContent?.trim()
+      setProduct((prevProduct) => ({
+        ...prevProduct,
+        rating: parseFloat(rating)
+      }))
+    }
+  }
+
+  const getRatingCount = () => {
+    const element = document.querySelector(
+      "span#acrCustomerReviewText"
+    ) as HTMLSpanElement
+
+    if (element) {
+      const ratingCount = element.innerText.split(" ")[0].replace(",", "")
+      setProduct((prevProduct) => ({
+        ...prevProduct,
+        rating_count: parseInt(ratingCount)
+      }))
+    }
+  }
+
+  const getStars = () => {
+    const productStars: ProductStar = {}
+
+    const starRatings: { [key: string]: string } = {
+      five: "a[href*='filterByStar=five_star']",
+      four: "a[href*='filterByStar=four_star']",
+      three: "a[href*='filterByStar=three_star']",
+      two: "a[href*='filterByStar=two_star']",
+      one: "a[href*='filterByStar=one_star']"
+    }
+
+    Object.keys(starRatings).forEach((key) => {
+      const starElement = document.querySelector(
+        starRatings[key]
+      ) as HTMLAnchorElement
+
+      if (starElement) {
+        const percentageText = starElement.textContent?.match(/(\d+)%/)?.[1]
+
+        if (percentageText) {
+          productStars[key as keyof ProductStar] = parseInt(percentageText, 10)
+        }
+      }
+    })
+
+    setProduct((prevProduct) => ({
+      ...prevProduct,
+      stars: productStars
+    }))
+  }
+
+  const getProductURL = () => {
+    setProduct((prevProduct) => ({
+      ...prevProduct,
+      product_url: window.location.href
+    }))
+  }
+
   useEffect(() => {
     getAsin()
+    getName()
     getCategory()
+    getDescription()
+    getPrice()
+    getStars()
+    getRating()
+    getImageUrl()
+    getRatingCount()
+    getProductURL()
   }, [])
 
   return (
@@ -67,6 +185,16 @@ export const ProductDetailMain = () => {
         <span>{product.rating}</span>
       </div>
       <div className="mb-4">
+        <span className="font-bold">Star: </span>
+        <span className="flex items-center gap-4">
+          <span>5: {product.stars?.five}%</span>
+          <span>4: {product.stars?.four}%</span>
+          <span>3: {product.stars?.three}%</span>
+          <span>2: {product.stars?.two}%</span>
+          <span>1: {product.stars?.one}%</span>
+        </span>
+      </div>
+      <div className="mb-4">
         <span className="font-bold">Rating Count: </span>
         <span>{product.rating_count}</span>
       </div>
@@ -77,6 +205,14 @@ export const ProductDetailMain = () => {
       <div className="mb-4">
         <span className="font-bold">Category: </span>
         <span>{product.category}</span>
+      </div>
+      <div className="mb-4">
+        <span className="font-bold">Product URL: </span>
+        <span>{product.product_url}</span>
+      </div>
+      <div className="mb-4">
+        <span className="font-bold">Image URL: </span>
+        <span>{product.image_url}</span>
       </div>
       <div className="mb-4">
         <span className="font-bold">Description: </span>
